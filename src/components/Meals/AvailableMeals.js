e8913455377aa17300e16d86d782aa8659252b5a
Antara Khan <khan.antara753@gmail.com>
2022-08-15 15:43:27 -0400

    cart output v2

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -31,7 +31,13 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
     const mealsList = DUMMY_MEALS.map((meal) => (
-        <MealItem  id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />
+        <MealItem  
+          id={meal.id} 
+          key={meal.id} 
+          name={meal.name} 
+          description={meal.description} 
+          price={meal.price} 
+        />
     ));
 
     return(
